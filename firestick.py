e9422171aff29170780c4983fe796a2af7e8e105
bradart <bradart.ink@gmail.com>
2016-08-28 03:39:16 -0500

    fixing messy code. I hate text editors. Kill me.

--- firestick.py
+++ firestick.py
@@ -6,19 +6,15 @@ yesno='n' #Initializing the ip verification variable
 
 #defining the function that actually installs kodi
 def installkodi(addy):
-	raw_input('Ok, here we go. This will probably take a minute\n\n\n\n\n\n\n\n\n\n\nPRESS ENTER')
+	raw_input('Ok, here we go. This will probably take a minute\n\n\nPRESS ENTER')
 	subprocess.call('adb kill-server',shell=True)
 	subprocess.call('adb connect '+addy,shell=True)
 	subprocess.call('adb install kodi.apk',shell=True)
-	raw_input('\n\n\n\n\n\n\n\n\n\nProvided no errors happened there, you should be good to go. Have a good one.\n\n\n\n\n\n\n\n\n\n\nPlease press enter to close the program')
+	raw_input('\n\n\nProvided no errors happened there, you should be good to go. Have a good one.\n\n\nPlease press enter to close the program')
 
 
-raw_input('Welcome to the program that will fuck up your Firestick\n\nPress enter to continue')
+raw_input('Welcome to the program that will fuck up your Firestick\n\n\nPress enter to continue')
 
-#while yesno!='y':
-#	ip=raw_input('First off, what is the IP address of your firestick on the network? \n\n(by the way, you should already have plugged it in and hooked it up to the network):')
-#	#figuring out whether or not you're stupid
-#	yesno=raw_input("So that\'s " + ip + "? (y/n):")
 
 testy=subprocess.check_output(['nmap','-sP','192.168.1.0/24'])
 listy=testy.split('\n')
@@ -29,14 +25,14 @@ for kindlehandle in listy:
 		themapcut1=themap.split('(')
 		themapcut2=themapcut1[1].split(')')
 		ip=themapcut2[0]
-		print 'Looks like your firestick IP is '+ip+'. Does that seem right? \n\n\nDon\'t care. \n\n\n\n\nWe''re going for it'
+		print '\n\n\nLooks like your firestick IP is '+ip+'. Does that seem right? \n\n\nDon\'t care. \n\n\nWe''re going for it'
 
 
 	
-dly=raw_input('Do you need to download the apk file to install? (y/n):')
+dly=raw_input('\n\n\nDo you need to download the apk file to install? (y/n):')
 if dly=='y':
-	print 'attempting a thing'
+	print '\n\n\nattempting a thing'
 	urllib.urlretrieve('http://mirrors.kodi.tv/releases/android/arm/kodi-16.1-Jarvis-armeabi-v7a.apk','kodi.apk')
 else:
-	raw_input('\n\n\n\nOk...If you already have the Kodi apk, it just has to be in the same directory as this program and named \'kodi.apk\'\n\n\n\n\n\n\press enter to continue')
+	raw_input('\n\n\nOk...If you already have the Kodi apk, it just has to be in the same directory as this program and named \'kodi.apk\'\n\n\npress enter to continue')
 installkodi(ip)
